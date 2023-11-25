4a8d0a3094d2ba0323b61db6f9d73a6f4b8f7e0c
omkeshborse <bomkesh25@gmail.com>
2023-11-25 19:31:51 +0530

    logout controller with jwt middleware

--- backend/controller/authControllers.js
+++ backend/controller/authControllers.js
@@ -93,7 +93,22 @@ const getuser = async (req, res, next) => {
   }
 };
 
-const logout = () =>{
-
-}
-module.exports = { signup, signin, getuser ,logout };
+const logout = (req,res) => {
+  try {
+    const cookieOption = {
+      expires: new Date(),
+      httpOnly: true,
+    };
+    res.cookie("token", null, cookieOption);
+    res.status(200).json({
+      success: true,
+      message: "Successfully logout",
+    });
+  } catch (error) {
+    return res.status(400).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+module.exports = { signup, signin, getuser, logout };
--- backend/router/authRoutes.js
+++ backend/router/authRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { signup, signin, getuser } = require("../controller/authControllers.js");
+const { signup, signin, getuser ,logout } = require("../controller/authControllers.js");
 const jwtAuth = require("../middleware/jwtAuth.js");
 const authRouter = express.Router();
 
 authRouter.post("/signup", signup);
 authRouter.post("/signin", signin);
 authRouter.get("/user", jwtAuth,getuser);
+authRouter.get("/logout" , jwtAuth,logout )
 module.exports = authRouter;
